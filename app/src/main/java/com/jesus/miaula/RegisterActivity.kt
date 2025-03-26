4c3a4e339a16c1d0632cea53ecf54dbeed63d249
Jesus Zenteno <jedrzr24@gmail.com>
2025-03-25 19:06:15 -0600

    actividad de registro layout

---
+++ app/src/main/java/com/jesus/miaula/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package com.jesus.miaula
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.jesus.miaula.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+    }
+
+}
